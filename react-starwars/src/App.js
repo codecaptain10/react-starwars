27a85c847220e1d7423f5813152f9d338972ae10
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-30 18:27:43 +0100

    Create routing in navbar

--- react-starwars/src/App.js
+++ react-starwars/src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
-import HomePage from "./components/HomePage";
 import Nav from "./components/Nav";
+import HomePage from "./components/HomePage";
+import HeroesPage from "./components/HeroesPage";
+import MoviesPage from "./components/MoviesPage";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      <Nav />
-      <HomePage />
-    </div>
+    <Router>
+      <div className='App'>
+        <Nav />
+
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route exact path='/heroes' element={<HeroesPage />} />
+          <Route exact path='/movies' element={<MoviesPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- react-starwars/src/components/Nav.js
+++ react-starwars/src/components/Nav.js
@@ -1,17 +1,31 @@
 import React from "react";
 import logo from "../img/sw_logo.png";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const navLinksStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
+
   return (
     <div className='navbar'>
       <div className='navbar-logo'>
-        <img src={logo}></img>
+        <Link to='/'>
+          <img src={logo}></img>
+        </Link>
       </div>
 
       <ul>
-        <li>HOME</li>
-        <li>HEROES</li>
-        <li>MOVIES</li>
+        <Link style={navLinksStyle} to='/'>
+          <li>HOME</li>
+        </Link>
+        <Link style={navLinksStyle} to='/heroes'>
+          <li>HEROES</li>
+        </Link>
+        <Link style={navLinksStyle} to='/movies'>
+          <li>MOVIES</li>
+        </Link>
       </ul>
     </div>
   );
