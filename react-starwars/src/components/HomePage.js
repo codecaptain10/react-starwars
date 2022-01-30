c0acf7caf7b0b3b699749b67c92b264fa352a165
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-30 23:12:23 +0100

    Create and design home page

--- react-starwars/src/components/HomePage.js
+++ react-starwars/src/components/HomePage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Home1 from "../img/home1.jpg";
+import Home2 from "../img/home2.jpg";
+import Home3 from "../img/home3.jpg";
 
 function HomePage() {
   return (
     <div className='home-page'>
-      <h1> Home Page</h1>
+      <section className='home-section1'>
+        <h1> Welcome in Star Wars World!</h1>
+        <div className='img-banner'>
+          <img src={Home1} alt=''></img>
+          <img src={Home2} alt=''></img>
+          <img src={Home3} alt=''></img>
+        </div>
+        <p>A long time ago in a galaxy far, far away .... Do you know that?</p>
+      </section>
     </div>
   );
 }
